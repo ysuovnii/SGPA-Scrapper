503cfffaab181e5bc332f5b26bec5b8aed321d81
ysuovnii <ysuovnii@gmail.com>
2026-01-18 14:43:52 +0530

    Optimised Tool

--- tool.py
+++ tool.py
@@ -1,14 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+import time 
 
-branchList = ['c', 'i', 'b', 't', 'e', 'm', 'v']
-sem = 2
-filename = f'./Result/Sem_{sem}_Result.csv'
+student_pass = []
+student_fail = []
 
-students = []
-
-roll_ranges = [(sem*1000+1, sem*1000+99), (sem*1000+101, sem*1000+199)]
+branch_list = ['c', 'i', 'b', 't', 'e', 'm', 'v']
+sem = 1
+roll_range = [(sem*1000+1, sem*1000+99), (sem*1001+101, sem*1000+199)]
 
 branch_map = {
     'c': 'Computer Science',
@@ -20,57 +20,104 @@ branch_map = {
     'v': 'Civil',
 }
 
-for branch in branchList:
-    branchName = branch_map.get(branch, 'Unknown Branch')
+def fetch(URL, branch):
 
-    for start, end in roll_ranges:
-        for roll in range(start, end):
-            url = f"http://results.ietdavv.edu.in/DisplayStudentResult?rollno=24{branch}{roll}&typeOfStudent=Regular"
+    try:
+        res = requests.get(URL, timeout=5)
+        soup = BeautifulSoup(res.text, 'html.parser')
 
-            response = requests.get(url)
-            soup = BeautifulSoup(response.text, 'html.parser')
+        nameTag = soup.find(string="Student Name")
+        rollTag = soup.find(string="Roll Number")
+        verdictTag = soup.find(string="Result")
+        cgTag = soup.find(string="SGPA")
 
-            nameTag = soup.find(text="Student Name")
-            sgpaTag = soup.find(text='SGPA')
-            rollNumberTag = soup.find(text='Roll Number')
+        if nameTag and rollTag and verdictTag and cgTag:
+            name = nameTag.find_next('td').text.strip()
+            roll = rollTag.find_next('td').text.strip()
+            verdict = verdictTag.find_next('td').text.strip()
+            cg = float(cgTag.find_next('td').text.strip())
 
-            if nameTag and sgpaTag and rollNumberTag:
-                name = nameTag.find_next('td').text.strip()
-                sgpa = float(sgpaTag.find_next('td').text.strip())
-                rollNumber = rollNumberTag.find_next('td').text.strip()
+            record = {
+                "branch" : branch, 
+                "name": name,
+                "roll": roll,
+                "verdict": verdict,
+                "cg": cg
+            }
 
-                students.append({
-                    "branch": branchName,
-                    "roll": rollNumber,
-                    "name": name,
-                    "sgpa": sgpa
-                })
+            if verdict == "Fail":
+                student_fail.append(record)
+            else :
+                student_pass.append(record)
+            
+            # print(record)
 
-                print(f"Saved: {branchName} {rollNumber} {name} : {sgpa}")
-            else:
-                print(f"Result not found for roll: 24{branch}{roll}")
+    except requests.exceptions.Timeout:
+        print(f"Timeout: {URL}")
 
-students_sorted = sorted(students, key=lambda x: x['sgpa'], reverse=True)
+    except Exception as e:
+        print(f"Error: {e}")
 
-rank = 1
-prev_sgpa = None
-for i, student in enumerate(students_sorted):
-    if prev_sgpa is None or student['sgpa'] < prev_sgpa:
-        rank = i + 1
-    student['rank'] = rank
-    prev_sgpa = student['sgpa']
 
-with open(filename, mode="w", newline="", encoding="utf-8") as f:
-    writer = csv.writer(f)
-    writer.writerow(["Rank", "Branch", "Roll Number", "Name", "SGPA"])
+    
+def generateURL():
+    total = len(branch_list) * sum(end - start for start, end in roll_range)
+    done = 0
+    for branch in branch_list:
+        branchName = branch_map.get(branch, 'Unknown Branch')
+        for start, end in roll_range:
+            for roll in range(start, end):
+                url = f"https://results.ietdavv.edu.in/DisplayStudentResult?rollno=24{branch}{roll}&typeOfStudent=Regular"
 
-    for s in students_sorted:
-        writer.writerow([
-            s['rank'],
-            s['branch'],
-            s['roll'],
-            s['name'],
-            s['sgpa']
-        ])
+                fetch(url, branchName)
+                done += 1
+                print(f"[{done}/{total}] fetched", end="\r")
+                time.sleep(0.3)
 
-print("CSV file created successfully!")
+def rank_students():
+    all_students = student_pass + student_fail
+
+    all_students.sort(
+        key=lambda x: (x["verdict"] == "Fail", -x["cg"])
+    )
+
+    rank = 1
+    prev_key = None  
+
+    for idx, student in enumerate(all_students):
+        current_key = (student["verdict"], student["cg"])
+
+        if current_key != prev_key:
+            rank = idx + 1
+
+        student["rank"] = rank
+        prev_key = current_key
+
+    return all_students
+
+def write_csv(all_students):
+    with open(f"Sem_{sem}_Result.csv", "w", newline="") as f:
+        fieldnames = ["rank", "branch", "name", "roll", "cg", "verdict"]
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for s in all_students:
+            writer.writerow(s)
+
+if __name__ == "__main__":
+    start_time = time.time()
+    generateURL()
+    print()
+
+    all_students = rank_students()
+
+    print(f"\nTotal students: {len(all_students)}")
+    print(f"Pass: {len(student_pass)}")
+    print(f"Fail: {len(student_fail)}")
+
+    write_csv(all_students)
+
+    print(f"\nExecution Time: {time.time() - start_time:.2f} seconds")
+
+     
+
