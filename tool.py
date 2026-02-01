28173f5675800fc2d9e2643966999110e6fc8036
ysuovnii <ysuovnii@gmail.com>
2026-02-02 01:22:29 +0530

    Updated tool

--- tool.py
+++ tool.py
@@ -5,21 +5,23 @@ import time
 
 session = requests.Session()
 session.headers.update({
-    #env
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
                   "AppleWebKit/537.36 (KHTML, like Gecko) "
                   "Chrome/143.0.0.0 Safari/537.36"
 })
 
-
 student_pass = []
 student_fail = []
 
+# 24 i 3 0 81  
+# {batch} {branch} {sem} {section} {roll}
 
-#env 
-branch_list = ['c', 'i', 'b', 't', 'e', 'm', 'v']
-sem = 3
-roll_range = [(sem*1000+1, sem*1000+90), (sem*1001+101, sem*1000+190)]
+batch = 24 
+batch_LE = 25
+branch_list = ['c', 'i', 'b', 't', 'e', 'm', 'v'] 
+sem = 3 
+roll_range = [(sem*1000+1, sem*1000+90), (sem*1000+101, sem*1000+190)]
+roll_range_LE = [(sem*1000+90, sem*1000+99), (sem*1000+190, sem*1000+199)]
 branch_map = {
     'c': 'Computer Science',
     'i': 'Information Technology',
@@ -55,6 +57,8 @@ def fetch(URL, branch, retries):
                     "cg": cg
                 }
 
+                print(f"{roll} {name} : {cg} {verdict}")
+
                 if verdict == "Fail":
                     student_fail.append(record)
                 else :
@@ -71,23 +75,52 @@ def fetch(URL, branch, retries):
             break  
 
     print(f"Failed to fetch after {retries} attempts: {URL}")
-
-
-
-    
+   
 def generateURL():
     total = len(branch_list) * sum(end - start for start, end in roll_range)
     done = 0
     for branch in branch_list:
         branchName = branch_map.get(branch, 'Unknown Branch')
-        for start, end in roll_range:
-            for roll in range(start, end):
-                url = f"https://results.ietdavv.edu.in/DisplayStudentResult?rollno=23{branch}{roll}&typeOfStudent=Regular"
+        if branch in ['b', 'e', 'm', 'v']: 
+            for roll in range(sem*1000+1, sem*1000+90):
+                url = f"https://results.ietdavv.edu.in/DisplayStudentResult?rollno={batch}{branch}{roll}&typeOfStudent=Regular"
 
                 fetch(url, branchName, 3)
                 done += 1
                 print(f"[{done}/{total}] fetched", end="\r")
                 time.sleep(0.3)
+        else : 
+            for start, end in roll_range:
+                for roll in range(start, end):
+                    url = f"https://results.ietdavv.edu.in/DisplayStudentResult?rollno={batch}{branch}{roll}&typeOfStudent=Regular"
+
+                    fetch(url, branchName, 3)
+                    done += 1
+                    print(f"[{done}/{total}] fetched", end="\r")
+                    time.sleep(0.3)
+
+def generateURL_LE():
+    total = len(branch_list) * sum(end - start for start, end in roll_range_LE)
+    done = 0
+    for branch in branch_list:
+        branchName = branch_map.get(branch, 'Unknown Branch')
+        if branch in ['b', 'e', 'm', 'v']: 
+            for roll in range(sem*1000+90, sem*1000+99):
+                url = f"https://results.ietdavv.edu.in/DisplayStudentResult?rollno={batch_LE}{branch}{roll}&typeOfStudent=Regular"
+
+                fetch(url, branchName, 3)
+                done += 1
+                print(f"[{done}/{total}] fetched", end="\r")
+                time.sleep(0.3)
+        else : 
+            for start, end in roll_range_LE:
+                for roll in range(start, end):
+                    url = f"https://results.ietdavv.edu.in/DisplayStudentResult?rollno={batch_LE}{branch}{roll}&typeOfStudent=Regular"
+
+                    fetch(url, branchName, 3)
+                    done += 1
+                    print(f"[{done}/{total}] fetched", end="\r")
+                    time.sleep(0.3)    
 
 def rank_students():
     all_students = student_pass + student_fail
@@ -111,7 +144,7 @@ def rank_students():
     return all_students
 
 def write_csv(all_students):
-    with open(f"Sem_{sem}_Result.csv", "w", newline="") as f:
+    with open(f"{batch}_Sem_{sem}_Result.csv", "w", newline="") as f:
         fieldnames = ["rank", "branch", "name", "roll", "cg", "verdict"]
         writer = csv.DictWriter(f, fieldnames=fieldnames)
 
